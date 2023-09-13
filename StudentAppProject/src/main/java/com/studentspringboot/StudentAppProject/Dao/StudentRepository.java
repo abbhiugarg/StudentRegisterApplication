6a7b919d70a2b868e04c34e8cff1eda26b083fb7
Abhiu Garg <abhiu.garg@qtsolv.com>
2023-09-13 14:31:28 +0530

    commit changes

--- StudentAppProject/src/main/java/com/studentspringboot/StudentAppProject/Dao/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.studentspringboot.StudentAppProject.Dao;
-
-import com.studentspringboot.StudentAppProject.entities.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Long> {
-
-
-}
